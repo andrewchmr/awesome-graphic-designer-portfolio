89b692dcf15aa8f32673e0a7e062e5fc61db9932
Andrzej Chemerinskiy <andrzej.chemerinskiy@blue-veery.com>
2019-12-05 13:31:40 +0100

    Add footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import './Footer.scss';
+
+export const Footer = () => {
+    return <footer className={'footer'}>
+        <p className={'footer__text'}>All content copyright Vernal Bloom 2019 ©</p>
+    </footer>
+};
