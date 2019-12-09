00c050873e26af71581e7332dbc12b53d8069118
Andrzej Chemerinskiy <andrzej.chemerinskiy@blue-veery.com>
2019-12-09 11:22:21 +0100

    Cleanup header styles, add links, active tab handler

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import './Header.scss';
 import {Link} from "react-router-dom";
 
+enum Tab {
+    WORK,
+    ABOUT
+}
+
 export const Header = () => {
+    const [activeTab, setActiveTab] = useState(Tab.WORK);
+
+    function getClassNameForTab(tab: Tab): string {
+        return `header__tab ${activeTab === tab && 'header__tab--active'}`;
+    }
+
     return <header className="header">
         <nav>
             <input className="header__menu-button" type="checkbox" id="menu-button"/>
             <label className="header__menu-icon" htmlFor="menu-button"><span/></label>
             <ul className="header__nav-sub">
-                <Link className="header__logo" style={{zIndex: 1000}} to={'/'}/>
-                <li className="header__tab header__tab--active"><Link to={'/work'}>Work</Link></li>
-                <li className="header__tab"><Link to="/about">About</Link></li>
+                <Link onClick={() => setActiveTab(Tab.WORK)} className="header__logo" style={{zIndex: 1000}}
+                      to={'/'}/>
+                <li className={getClassNameForTab(Tab.WORK)}>
+                    <Link onClick={() => setActiveTab(Tab.WORK)} to={'/'}>Work</Link>
+                </li>
+                <li className={getClassNameForTab(Tab.ABOUT)}>
+                    <Link onClick={() => setActiveTab(Tab.ABOUT)} to="/about">About</Link>
+                </li>
             </ul>
         </nav>
     </header>
