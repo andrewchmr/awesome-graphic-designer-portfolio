aa1798be3f4a2257d281682cf613de45de98971c
Andrzej Chemerinskiy <andrzej.chemerinskiy@blue-veery.com>
2020-01-01 20:36:45 +0100

    Add dummy text to About

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -3,11 +3,22 @@ import './About.scss';
 
 export const About = () => {
     return <div className={'about'}>
-        <p className={'about__desc'}>Vernal Bloom is a Ukrainian artist based in London.</p>
-        <p className={'about__desc'}> Her bold, minimal style – often described as Pop Art meets OpArt – is
-            a striking lesson in the use of positive/negative space and colour.</p>
-        <p className={'about__desc'}> Her unmistakable style has established her as one of the UK’s most
-            sought after graphic artists. Vernal Bloom’s clients include The New Yorker, Vogue, BAFTA,
-            Sephora and Penguin Books, amongst many others.</p>
+        <div>
+            <p>Vernal Bloom is a Ukrainian artist based in Poland</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur distinctio iste nemo! Accusantium
+                dolorum nam optio quae quisquam recusandae sapiente. Ab accusantium deserunt error ipsa non placeat
+                recusandae, reiciendis tempora!</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad animi aut beatae consequatur cum
+                cumque deleniti, ea esse est, iure labore omnis pariatur placeat quibusdam repellat soluta ut vitae!</p>
+        </div>
+        <div>
+            <p>Contact me!</p>
+            <p>Email</p>
+            <p>vernal.bloom@gmail.com</p>
+            <p>Follow</p>
+            <p>icons</p>
+            <p>Website by</p>
+            <p>@andrewchmr</p>
+        </div>
     </div>
 };
