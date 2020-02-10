46cf731233074df7e15df28a7670163ba6fc4bbb
Andrzej Chemerinskiy <andrzej.chemerinskiy@blue-veery.com>
2020-02-10 16:57:24 +0100

    Update About

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -3,22 +3,10 @@ import './About.scss';
 
 export const About = () => {
     return <div className={'About'}>
-        <div>
-            <p>Vernal Bloom is a Ukrainian artist based in Poland</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur distinctio iste nemo! Accusantium
-                dolorum nam optio quae quisquam recusandae sapiente. Ab accusantium deserunt error ipsa non placeat
-                recusandae, reiciendis tempora!</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad animi aut beatae consequatur cum
-                cumque deleniti, ea esse est, iure labore omnis pariatur placeat quibusdam repellat soluta ut vitae!</p>
-        </div>
-        <div>
-            <p>Contact me!</p>
-            <p>Email</p>
-            <p>vernal.bloom@gmail.com</p>
-            <p>Follow</p>
-            <p>icons</p>
-            <p>Website by</p>
-            <p>@andrewchmr</p>
-        </div>
+        <p>Vernal Bloom is a Ukrainian artist based in Poland.</p>
+        <p>Contact me!</p>
+        <p>Email: <a className={'About__email'} href='mailto:vernalbloomart@gmail.com'>vernalbloomart@gmail.com</a></p>
+        <p>Website by <a target="_blank" className={'About__email'} href='https://github.com/andrewchmr'>@andrewchmr</a>
+        </p>
     </div>
 };
