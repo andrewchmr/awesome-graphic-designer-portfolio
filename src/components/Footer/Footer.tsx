21d73feadcb0d096ba80e04e1b694dca7dca4d12
Andrzej Chemerinskiy <andrzej.chemerinskiy@blue-veery.com>
2020-02-18 11:21:11 +0100

    Use NavLink in Header, update html structure

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -12,7 +12,7 @@ export const Footer = () => {
             {/*   <li><a href="" target="_blank"><i className="icon-linkedin-squared"/></a></li>*/}
             <li><a href="https://www.behance.net/vernalbloom1" target="_blank" rel="noopener noreferrer">
                 <i className="icon-behance"/></a></li>
-            <p className='Footer__copyright'>All content copyright Vernal Bloom 2020 ©</p>
         </ul>
+        <p className='Footer__copyright'>All content copyright Vernal Bloom 2020 ©</p>
     </footer>
 };
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,40 +1,23 @@
-import React, {useState} from "react";
+import React from "react";
 import './Header.scss';
-import {Link} from "react-router-dom";
-
-enum Tab {
-    WORK = '/',
-    ABOUT = '/about'
-}
-
-const getTabByUrl = () => {
-    const path = window.location.pathname;
-    if (path.match('^\\/work[a-zA-Z]*')) {
-        return Tab.WORK;
-    } else if (path.match('^\\/about[a-zA-Z]*')) {
-        return Tab.ABOUT;
-    }
-    return path;
-};
+import {NavLink} from "react-router-dom";
 
 export const Header = () => {
-    const [activeTab, setActiveTab] = useState(getTabByUrl());
-
-    const getClassNameForTab = (tab: Tab): string => {
-        return `Header__tab ${activeTab === tab && 'Header__tab--active'}`;
-    };
-
     return <header className="Header">
         <nav>
             <input className="Header__menu-button" type="checkbox" id="menu-button"/>
             <label className="Header__menu-icon" htmlFor="menu-button"><span/></label>
             <ul className="Header__nav-sub">
-                <Link onClick={() => setActiveTab(Tab.WORK)} className="Header__logo" to={'/'}/>
-                <li className={getClassNameForTab(Tab.WORK)}>
-                    <Link onClick={() => setActiveTab(Tab.WORK)} to={'/'}>Work</Link>
+                <li><NavLink className="Header__logo" to={'/'}/></li>
+                <li className={'Header__tab'}>
+                    <NavLink exact to={'/'}
+                             className={'Header__link'}
+                             activeClassName="Header__link--active">Work</NavLink>
                 </li>
-                <li className={getClassNameForTab(Tab.ABOUT)}>
-                    <Link onClick={() => setActiveTab(Tab.ABOUT)} to="/about">About</Link>
+                <li className={'Header__tab'}>
+                    <NavLink to="/about"
+                             className={'Header__link'}
+                             activeClassName="Header__link--active">About</NavLink>
                 </li>
             </ul>
         </nav>
