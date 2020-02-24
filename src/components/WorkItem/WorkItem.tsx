60fc056bf8753246ce3e85e246b656f6c54bed62
Andrzej Chemerinskiy <andrzej.chemerinskiy@blue-veery.com>
2020-02-24 12:31:14 +0100

    Add placeholder onload to WorkItem

--- src/components/WorkItem/WorkItem.tsx
+++ src/components/WorkItem/WorkItem.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import './WorkItem.scss';
 import {Link} from "react-router-dom";
 import {toSeoUrl} from "../../utils/toSeoUrl";
@@ -15,15 +15,26 @@ interface WorkItemProps {
 }
 
 export const WorkItem = ({workItem, onClick}: WorkItemProps) => {
+    const [imageLoaded, setImageLoaded] = useState<boolean>(false);
+
+    const showImageOverlay = () => {
+        return <div className={'WorkItem__desc-overlay'}>
+            <div className={'WorkItem__desc'}>{workItem.imageName}</div>
+        </div>;
+    };
+
+    const showPlaceholder = () => {
+        return <div className={'WorkItem__placeholder'}/>;
+    };
+
     return <div className="WorkItem">
         <Link className={'WorkItem__image-wrap'} onClick={() => onClick(workItem)}
               to={`/work/${toSeoUrl(workItem.imageName)}`}>
-            <img className={'WorkItem__image'}
+            <img className={'WorkItem__image'} onLoad={() => setImageLoaded(true)}
                  src={require(`../../images/${workItem.fileName}-sm.jpg`)}
                  alt={`Vernal Bloom - ${workItem.imageName}`}/>
-            <div className={'WorkItem__desc-overlay'}>
-                <div className={'WorkItem__desc'}>{workItem.imageName}</div>
-            </div>
+            {imageLoaded && showImageOverlay()}
+            {!imageLoaded && showPlaceholder()}
         </Link>
     </div>
 };
