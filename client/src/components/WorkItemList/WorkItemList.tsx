81d5ade52822181630c6e463aed75aec3b4c148d
Andrzej Chemerinskiy <andrzej.chemerinskiy@blue-veery.com>
2020-03-07 11:23:29 +0100

    Add Loader to WorkItemList

--- client/src/components/WorkItemList/WorkItemList.tsx
+++ client/src/components/WorkItemList/WorkItemList.tsx
@@ -1,10 +1,13 @@
 import {WorkItem} from "./WorkItem/WorkItem";
 import React from "react";
 import './WorkItemList.scss';
+import {Loader} from "../Loader/Loader";
 
 export const WorkItemList = ({workItemList}: { workItemList: WorkItem[] }) => {
-    return <section className="WorkItemList">
-        {workItemList.map((workItem: WorkItem) =>
-            <WorkItem key={workItem._id} {...workItem}/>)}
-    </section>
+    if (workItemList.length > 0) {
+        return <section className="WorkItemList">{workItemList.map((workItem: WorkItem) =>
+            <WorkItem key={workItem._id} {...workItem}/>)}</section>
+    } else {
+        return <Loader/>;
+    }
 };
