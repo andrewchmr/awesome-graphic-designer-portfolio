c8739e17285648ecb30dd6662319bea46a316265
Andriy Chemerinskiy <andrzej.chem@gmail.com>
2020-04-08 20:12:03 +0200

    Update About desc, minor fixes

--- client/src/components/About/About.tsx
+++ client/src/components/About/About.tsx
@@ -3,11 +3,29 @@ import './About.scss';
 
 export const About = () => {
     return <div className={'About'}>
-        <p>Vernal Bloom is a Ukrainian artist based in Poland.</p>
-        <p>Contact me!</p>
-        <p>Email: <a className={'About__email'} href='mailto:vernalbloomart@gmail.com'>vernalbloomart@gmail.com</a></p>
-        <p>Website by <a target="_blank" rel="noopener noreferrer" className={'About__email'}
-                         href='https://github.com/andrewchmr'>@andrewchmr</a>
+        <div className={'About__designer-desc'}>
+            <img className={'About__image'} alt="Vernal Bloom - Self Portrait"
+                 src="https://res.cloudinary.com/htn4pmlv5/image/upload/c_scale,w_800/v1584045962/ox0uy637qddzbljthihx.jpg"/>
+            <div className={'About__desc'}>
+            <span>I am a Ukrainian artist based in Poland. I have been studying visual arts for 5 years and chose graphic
+                design as my specialization. I started drawing in the early childhood as it was my biggest passion.
+                Since
+                than I wanted to develop my skills, tried new techniques and switched from traditional art to digital
+                media.
+                Now I am interested in vector&bitmap graphic. I create logos, illustrations and posters. Also I'm fond
+                of
+                motion and web design and still looking for my own style. I'm always open for collaborations and new
+                challenges :)</span>
+                <div className={'About__contact'}>
+                    <div>Contact me!</div>
+                    <div>Email: <a className={'About__email'}
+                                   href='mailto:vernalbloomart@gmail.com'>vernalbloomart@gmail.com</a></div>
+                </div>
+            </div>
+        </div>
+        <p className={'About__author-desc'}>Website by <a target="_blank" rel="noopener noreferrer"
+                                                          className={'About__email'}
+                                                          href='https://github.com/andrewchmr'>@andrewchmr</a>
         </p>
     </div>
 };
