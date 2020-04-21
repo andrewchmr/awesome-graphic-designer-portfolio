8458d9134db89dae690b1a06aa42aa47a92f1831
Andriy Chemerinskiy <andrzej.chem@gmail.com>
2020-04-21 15:43:48 +0200

    Use custom serviceWorker conf

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,8 +4,23 @@ import './variables.scss';
 import './index.scss';
 import './fontello-66c86d21/css/fontello.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const isLocalhost = Boolean(
+    window.location.hostname === 'localhost' ||
+    window.location.hostname === '[::1]' ||
+    window.location.hostname.match(
+        /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
+);
 
-serviceWorker.register();
+if (!isLocalhost && 'serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/offline.js', {scope: '/'})
+        .then(() => {
+            console.log('Service Worker Registered');
+        });
+    navigator.serviceWorker.ready.then(() => {
+        console.log('Service Worker Ready');
+    });
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
